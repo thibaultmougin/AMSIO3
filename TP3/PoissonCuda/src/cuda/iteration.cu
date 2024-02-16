835988ed3084bb24f84d9a88f29a2cc1149bc910
Thibault Mougin <thibault.mougin@ensta-paris.fr>
2024-02-16 15:17:02 +0100

    insh

--- TP3/PoissonCuda/src/cuda/iteration.cu
+++ TP3/PoissonCuda/src/cuda/iteration.cu
@@ -5,7 +5,34 @@
 #include "timer.hxx"
 #include "iteration.hxx"
 
-// A completer : definition du noyau
+__global__
+void noyau_iteration(double *v, double* u, double dt){
+    int i = threadIdx.x + blockIdx.x * blockDim.x;
+    int j = threadIdx.y + blockIdx.y * blockDim.y;
+    int k = threadIdx.z + blockIdx.z * blockDim.z;
+    int p;
+    double du1;
+
+    if (i>0 && i<d_n[0]-1 && j>0 && j<d_n[1]-1 && k>0 && k<d_n[2]-1)
+    {
+    p = i + d_n[0] * (j + k*d_n[1]);
+    du1 = (-2 * u[p] + u[p+1] + u[p-1]) * d_lambda[0]
+            + (-2 * u[p] + u[p+d_n[0]] + u[p-d_n[0]]) * d_lambda[1]
+            + (-2 * u[p] + u[i + d_n[0] * (j + (k+1)*d_n[1])] + u[i + d_n[0] * (j + (k-1)*d_n[1])]) * d_lambda[2];
+    
+
+    double x = d_xmin[0] + i * d_dx[0];
+    double y = d_xmin[1] + j * d_dx[1];
+    double z = d_xmin[2] + k * d_dx[2];
+    double du2 = force(x, y, z);
+
+    double du = dt * (du1 + du2);
+    v[p] = u[p] + du;
+
+  }
+}
+
+
 
 void iteration(
     Values & v, Values & u, double dt, int n[3],
@@ -13,5 +40,12 @@ void iteration(
     int jmin, int jmax,
     int kmin, int kmax)
 {
-// A completer : appel du noyau
+    dim3 dimBlock(8,8,8);
+    dim3 dimGrid(int(ceil(n[0]/double(dimBlock.x))),
+               int(ceil(n[1]/double(dimBlock.y))),
+               int(ceil(n[2]/double(dimBlock.z))));
+
+    noyau_iteration<<<dimGrid, dimBlock>>>(v.dataGPU(),u.dataGPU(),dt);
+    cudaDeviceSynchronize();
+    CUDA_CHECK_KERNEL();
 }
--- TP3/PoissonCuda/src/cuda/user.cu
+++ TP3/PoissonCuda/src/cuda/user.cu
@@ -1,2 +1,16 @@
+#include "dim.cuh"
+#include "cuda_check.cuh"
+#include "user.cuh"
 
-// A completer
+#include "timer.hxx"
+#include "iteration.hxx"
+
+__device__
+double cond_ini(double x, double y, double z);
+
+__device__
+double cond_lim(double x, double y, double z);
+
+__device__
+double force(double x, double y, double z);
+
